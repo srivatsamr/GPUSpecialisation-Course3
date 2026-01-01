c81eef6863c2cd225b831c96da41b97bc1e6c0ca
Srivatsa Mudumba Ramana <sri1mudumba@gmail.com>
2026-01-01 20:22:25 +0000

    Reorganise function definitions

--- include/edge_npp.h
+++ include/edge_npp.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <vector>
 #include <string>
+
+#include <cuda_runtime.h>
+#include <npp.h>
+#include <filesystem>
+#include <vector>
+#include <iostream>
+
+struct StreamContext {
+    cudaStream_t stream{};
+    Npp8u* d_src{};
+    Npp8u* d_dst{};
+    size_t srcPitch{};
+    size_t dstPitch{};
+};
+
+namespace fs = std::filesystem;
 
 void checkNppStatus(NppStatus status, const std::string& msg);
 std::vector<fs::path> load_images(const std::string& inputDir);
